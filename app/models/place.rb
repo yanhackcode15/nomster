30244a2f0ed17640f7e3a90d526a2f842978c78e
Yan Hu <iamyanhu@gmail.com>
2015-10-25 03:52:12 +0000

    Added validation for all fields

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,7 @@
 class Place < ActiveRecord::Base
-	validates :name, presence: true
+	validates :name, presence: true, length: { minimum: 3 }
+	validates :address, presence: true
+	validates :description, presence: true
+	
 	belongs_to :user
 end
