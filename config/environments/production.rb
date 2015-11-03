9a754ead4e6a86dee74c335eaee49fff5b5c01f4
Yan Hu <iamyanhu@gmail.com>
2015-11-03 16:09:25 +0000

    added mailgun to heroku' exit q quit  abort quit
    x

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,14 @@
 Nomster::Application.configure do
+  ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'nomster-yan-hu.heroku.com',
+  :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
+  
   config.action_mailer.default_url_options = { :host => 'nomster-yan-hu.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
